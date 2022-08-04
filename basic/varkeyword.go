e3bf703ee067d13b09df2a2eaba2fffda82ae8d4
Pratap kute <pratap.kute97@gmail.com>
2022-08-04 15:56:54 +0530

    basic of go like var and string

---
+++ basic/varkeyword.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//short declaration operator
+	// Declare a variable and assign a value of a certain type
+	x := 42
+	fmt.Println(x)
+	var y = 43
+	fmt.Println(y)
+}
