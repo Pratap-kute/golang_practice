62456fbb35f4e1a8d0880c1a3962116f5c3d06d1
Pratap kute <pratap.kute97@gmail.com>
2022-09-12 14:46:40 +0530

    formating

--- consts/Iota.go
+++ consts/Iota.go
@@ -1,17 +1,19 @@
 package main
+
 import "fmt"
-// iota increment the next veriable by one 
+
+// iota increment the next veriable by one
 const (
-	a=iota
+	a = iota
 	b
 	c
 )
 
-func main(){
+func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Printf("%T\n",a)
-	fmt.Printf("%T\n",b)
-	fmt.Printf("%T\n",c)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
 }
