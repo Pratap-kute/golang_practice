fba51f65e32f8dc57d91c3507c7a40b29b57b73c
Pratap kute <pratap.kute97@gmail.com>
2022-09-13 15:14:36 +0530

    printing type and untype constants

---
+++ exercise_2/constants.go
@@ -0,0 +1,14 @@
+/*
+Create TYPED and UNTYPED constants. Print the values of the constants.
+*/
+
+package main
+
+import "fmt"
+
+const a = "hi"
+const b int = 0
+
+func main() {
+	fmt.Println(a, b)
+}
