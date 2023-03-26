ea344e990e2cffb36586f87ae536dbc45d431281
Pratap Kute <pratap.kute@d10x.io>
2023-03-26 19:50:12 +0530

    function methods

---
+++ funtions/methods.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+
+// func (r receiver) identifier(parameters) (return(s)) { code }
+// method is a function with a receiver argument is most important
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+	sa1.speak()
+}
