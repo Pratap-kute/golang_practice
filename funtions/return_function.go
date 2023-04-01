7b2fbb817790859dfbd0a73ea0e7489736bc9183
Pratap Kute <pratap.kute@d10x.io>
2023-04-01 16:54:02 +0530

    understating the function returns

--- funtions/return_function.go
+++ funtions/return_function.go
@@ -5,9 +5,21 @@ import "fmt"
 func main() {
 	s1 := foo()
 	fmt.Println(s1)
+
+	x := bar()
+	fmt.Printf("%T\n", x)
+
+	i := x()
+	fmt.Println(i)
 }
 
 func foo() string {
-	s := "Hello from foo"
-	return s
+	// s := "Hello from foo"
+	return "Hello from foo"
+}
+
+func bar() func() int {
+	return func() int {
+		return 451
+	}
 }
