451aa23a907b38eedbd499a75ffd5ac434f416c3
Pratap Kute <pratap.kute@d10x.io>
2024-09-03 21:54:33 +0530

    new tests

--- learn-go-with-test/hello_test.go
+++ learn-go-with-test/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Amol")
-	want := "Hello Amol"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Amol")
+		want := "Hello Amol"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello World' when a empty string is supplied ", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello world"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
