1f7d83813f3e572fe593c8e0f688b63cd5e16a05
Pratap Kute <pratap.kute@d10x.io>
2024-10-05 20:50:00 +0530

    started using slice

--- learn-go-with-test/arrays-and-slices/sum.go
+++ learn-go-with-test/arrays-and-slices/sum.go
@@ -1,6 +1,6 @@
 package main
 
-func Sum(number [5]int) int {
+func Sum(number []int) int {
 	sum := 0
 	// we are using range returns two values - the index and the value.
 	// We are choosing to ignore the index value by using _ blank identifier.
