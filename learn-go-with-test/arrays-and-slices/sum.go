a1ca361702f28089771e7c248235607281a9072c
Pratap Kute <pratap.kute@d10x.io>
2024-10-06 13:17:57 +0530

    understating the make
    - how to make slice with length
    - also for the capacity

--- learn-go-with-test/arrays-and-slices/sum.go
+++ learn-go-with-test/arrays-and-slices/sum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Sum(number []int) int {
 	sum := 0
 	// we are using range returns two values - the index and the value.
@@ -8,4 +10,19 @@ func Sum(number []int) int {
 		sum += number
 	}
 	return sum
+}
+
+// "...[]int" is used to accept the any lenght of slice
+func SumAll(numberToSum ...[]int) []int {
+	lengthOfNumber := len(numberToSum)
+	capasityofNumber := cap(numberToSum)
+	fmt.Printf("capacity of slice %d \n", capasityofNumber)
+	// make allows you to create a slice with a starting capacity of the len of the numbersToSum we need to work through.
+	sums := make([]int, lengthOfNumber)
+
+	for i, numbers := range numberToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
