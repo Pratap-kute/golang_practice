dfd4127da2388b1d8f1cf05af8d7aa53f1892457
Pratap Kute <pratap.kute@d10x.io>
2024-10-23 19:51:38 +0530

    working with types and it's methods

--- learn-go-with-test/structs-methods-and-interfaces/shapes.go
+++ learn-go-with-test/structs-methods-and-interfaces/shapes.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
@@ -9,10 +11,10 @@ type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- learn-go-with-test/structs-methods-and-interfaces/shapes_test.go
+++ learn-go-with-test/structs-methods-and-interfaces/shapes_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
