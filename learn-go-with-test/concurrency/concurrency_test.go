66e4ccad5cb7d202c987a46e707da87ac1ffd098
Pratap kute <pratap.kute97@gmail.com>
2026-08-04 16:01:12 +0530

    feat: implement CheckWebsites function and associated tests for concurrent URL validation

---
+++ learn-go-with-test/concurrency/concurrency_test.go
@@ -0,0 +1,45 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave.net",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":         true,
+		"http://blog.gypsydave.net": true,
+		"waat://furhurterwe.geds":   false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	for b.Loop() {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
